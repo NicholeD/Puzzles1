01e7fb17cd167a643a46e992d19414df44051e16
NicholeD <nicholedavid@gmail.com>
2022-12-11 17:56:37 -0700

    MazeRunner implemented. Tests pass.

--- BreadthFirstSearch/MazeRunner/src/main/java/com/kenzie/breadthfirstsearch/mazerunner/MazeRunner.java
+++ BreadthFirstSearch/MazeRunner/src/main/java/com/kenzie/breadthfirstsearch/mazerunner/MazeRunner.java
@@ -2,12 +2,12 @@ package com.kenzie.breadthfirstsearch.mazerunner;
 
 import com.kenzie.breadthfirstsearch.mazerunner.model.MazePattern;
 import com.kenzie.breadthfirstsearch.mazerunner.model.MazeSpace;
+import com.kenzie.breadthfirstsearch.mazerunner.sharedmodel.Direction;
 import com.kenzie.breadthfirstsearch.mazerunner.sharedmodel.Node;
 import com.kenzie.breadthfirstsearch.mazerunner.utils.MazeGenerator;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
+import java.util.stream.Collectors;
 
 import static com.kenzie.breadthfirstsearch.mazerunner.SampleMazes.MAZE_ONE_EXIT;
 
@@ -15,6 +15,7 @@ import static com.kenzie.breadthfirstsearch.mazerunner.SampleMazes.MAZE_ONE_EXIT
  * Class for running through mazes.
  */
 public class MazeRunner {
+    private Direction direction;
 
     /**
      * Private constructor, as all methods are static.
@@ -38,6 +39,30 @@ public class MazeRunner {
      */
     public static Optional<MazeSpace> findClosestExit(MazePattern pattern) {
         Optional<Node<MazeSpace>> entrance = MazeGenerator.generateMaze(pattern);
+
+        if (entrance.isEmpty()) {
+            return Optional.empty();
+        }
+
+        Set<Node<MazeSpace>> visitedNodes = new HashSet<>();
+        Queue<Node<MazeSpace>> currentLevel = new LinkedList<>();
+        currentLevel.add(entrance.get());
+
+        while (!currentLevel.isEmpty()) {
+            Node<MazeSpace> currentNode = currentLevel.poll();
+
+            if (currentNode.getValue().isExit()) {
+                return Optional.ofNullable(currentNode.getValue());
+            } else {
+                visitedNodes.add(currentNode);
+
+                for (Node<MazeSpace> node : currentNode.getNeighbors()) {
+                    if (!visitedNodes.contains(node)) {
+                        currentLevel.add(node);
+                    }
+                }
+            }
+        }
         return Optional.empty();
     }
 
@@ -49,6 +74,44 @@ public class MazeRunner {
      */
     public static List<MazeSpace> findShortestPathToExit(MazePattern pattern) {
         Optional<Node<MazeSpace>> entrance = MazeGenerator.generateMaze(pattern);
+
+        if (entrance.isEmpty()) {
+            return Collections.emptyList();
+        }
+
+        Set<Node<MazeSpace>> visitedNodes = new HashSet<>();
+        Queue<List<Node<MazeSpace>>> currentLevel = new LinkedList<>();
+        Queue<List<Node<MazeSpace>>> nextLevel = new LinkedList<>();
+        List<Node<MazeSpace>> currentPath = new ArrayList<>();
+
+        currentPath.add(entrance.get());
+        currentLevel.add(currentPath);
+
+        while(!currentLevel.isEmpty()) {
+            currentPath = currentLevel.poll();
+            Node<MazeSpace> currentNode = currentPath.get(currentPath.size() - 1);
+            visitedNodes.add(currentNode);
+
+            if (currentNode.getValue().isExit()) {
+                return currentPath.stream()
+                        .map(Node::getValue)
+                        .collect(Collectors.toList());
+            } else {
+                for (Node<MazeSpace> node : currentNode.getNeighbors()) {
+                    if (!visitedNodes.contains(node)) {
+                        List<Node<MazeSpace>> path = new ArrayList<>(currentPath);
+                        path.add(node);
+                        nextLevel.add(path);
+                    }
+                }
+            }
+
+            if (currentLevel.isEmpty()) {
+                currentLevel = nextLevel;
+                nextLevel = new LinkedList<>();
+            }
+        }
+
         return Collections.emptyList();
     }
 }
